d0d25cb64ca2538df6bf38cd6df1e23046875c65
amir.ebrahimi <amirkingofnight@gmail.com>
2023-08-13 15:21:14 +0330

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBLYiWFd5NoG3oPgwAF99oW9Mo0scV8HKg",
+  authDomain: "netflix-clone-ad85d.firebaseapp.com",
+  projectId: "netflix-clone-ad85d",
+  storageBucket: "netflix-clone-ad85d.appspot.com",
+  messagingSenderId: "529999533294",
+  appId: "1:529999533294:web:48e1a3b91a4e816889cfff",
+};
